5b7652bc4475cbd8d1d05eee0a6b3b2ac538161a
Ing-ed <emidovichi@gmail.com>
2025-08-31 22:12:21 -0300

    funciona la discriminacion por errorConstancia en la respuesta

--- src/main/java/com/example/demo/ArcaDriver.java
+++ src/main/java/com/example/demo/ArcaDriver.java
@@ -1,32 +1,28 @@
 package com.example.demo;
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 
-import java.awt.*;
-import java.io.File;
 import java.io.FileReader;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.Gson;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+import org.springframework.stereotype.Component;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-/**
- * datos necesarios:
- * razon social
- * cuit
- * actividades (codigo)
- * codigo postal
- * jurisdiccion
- * responsabilidad frente al iva
- */
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-public class UI {
-    FetchDriver driver;// = new FetchDriver("20409378472")
+public class ArcaDriver {
+    private FetchDriver driver;// = new FetchDriver("20409378472")
     private List<String> outData;
-    private String OkConstancia(JsonObject persona){
+    public ArcaDriver(){
+        driver = new FetchDriver("20409378472");
+        outData = new ArrayList<String>();
+        driver.Authenticate();
+    }
+      private String OkConstancia(JsonObject persona){
         JsonObject datosGenerales = persona.get("datosGenerales").
         getAsJsonObject();
         JsonObject datosRegimenGral = persona.get("datosRegimenGeneral").
@@ -150,52 +146,4 @@ public class UI {
             return "Error";
         }
     }
-    public UI(){
-        driver = new FetchDriver("20409378472");
-        outData = new ArrayList<String>();
-        driver.Authenticate();
-        JFrame frame = new JFrame("Hola mundo");
-        frame.setSize(400,700);
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBorder(new EmptyBorder(20, 0, 0, 0));
-
-        JTextField openFile = new JTextField();
-        openFile.setMaximumSize(new Dimension(200,30));
-        openFile.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        JButton open = new JButton("Abrir");
-        open.setPreferredSize(new Dimension(70,30));
-        open.setAlignmentX(Component.CENTER_ALIGNMENT);
-        open.addActionListener(e -> this.Open(panel,openFile));
-
-        JTextArea viewData = new JTextArea();
-        viewData.setMaximumSize(new Dimension(300,500));
-        viewData.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        JTextField saveFile = new JTextField();
-        saveFile.setMaximumSize(new Dimension(200,30));
-        saveFile.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        JButton send = new JButton("Guardar");
-        send.setPreferredSize(new Dimension(70,30));
-        send.setAlignmentX(Component.CENTER_ALIGNMENT);
-        send.addActionListener(e -> GetCuit(openFile.getText()));
-        panel.setBorder(new EmptyBorder(20,0,20,0));
-
-
-
-        panel.add(openFile);
-        panel.add(Box.createRigidArea(new Dimension(20,5)));
-        panel.add(open);
-        // panel.add(Box.createVerticalGlue());
-        panel.add(viewData);
-        panel.add(saveFile);
-        panel.add(send);
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.pack();
-        frame.setVisible(true);
-    }
-    
 }
